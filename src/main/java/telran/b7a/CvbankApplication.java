e96ee8049833d4928f023d47025f59b1d52fc1a8
Nikita <nikan2110@gmail.com>
2022-11-23 14:19:54 +0200

    add war packaging

--- src/main/java/telran/b7a/CvbankApplication.java
+++ src/main/java/telran/b7a/CvbankApplication.java
@@ -2,13 +2,20 @@ package telran.b7a;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class CvbankApplication{
+public class CvbankApplication extends SpringBootServletInitializer{
 	
 
 	public static void main(String[] args) {
 		SpringApplication.run(CvbankApplication.class, args);
+	}
+	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(CvbankApplication.class);
 	}
 
 }
